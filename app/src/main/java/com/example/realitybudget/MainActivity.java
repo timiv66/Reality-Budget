534d7ec36687052266e0b6d7cca92c613bf572f9
Timi Verissimo <timiv9@gmail.com>
2024-09-11 21:31:12 -0400

    changed app icon

--- app/src/main/java/com/example/realitybudget/MainActivity.java
+++ app/src/main/java/com/example/realitybudget/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.realitybudget;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
 
